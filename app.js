e3cf08b9cb0cd6fa79d28c63a738fb910f5d508a
Adrian Ezequiel Martinez ∴ <adri.eze.martinez@gmail.com>
2023-09-21 20:53:32 +0200

    update api call

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/bienvenido/', (req, res) => {
+app.get('/bienvenido', (req, res) => {
   res.send('Hola!');
 });
 
-app.get('/alumno/', (req, res) => {
+app.get('/alumno', (req, res) => {
     res.send('Soy un alumno');
   });
 
